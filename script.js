a268aa71c49514b11dcd11f761695897f98b1273
HackerPrince4321 <princesoni8761969@gmail.com>
2025-10-05 18:38:29 +0530

    update

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function() {
 
   // typing text animation script
   var typed = new Typed(".typing", {
-    strings: ["Web Developer", " Game Developer", "Blogger", "Designer", "Freelancer", " Gamer"],
+    strings: ["Web Developer", "Data Analyst", "Designer", "Freelancer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
   });
 
   var typed = new Typed(".typing-2", {
-    strings: ["Web Developer", " Game Developer", "Blogger", "Designer", "Freelancer", " Gamer"],
+    strings: ["Web Developer", "Data Analyst", "Designer", "Freelancer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
